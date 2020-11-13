1c59751c82bf99434ba7f207d3dc0e92a26706b3
DinSV <63643468+DinSV@users.noreply.github.com>
2020-11-13 16:56:09 +0700

    Create Solution.java
    справочник / решебник

---
+++ Solution.java
@@ -0,0 +1,29 @@
+
+// фйл решебник / справочник
+/* Создай объект типа Cat (кот), объект типа Dog (собака),
+объект типа Fish (рыбка) и объект типа Woman.
+Присвой каждому животному владельца (owner).
+Питомцам нужны люди
+*/
+public class Solution {
+    public static void main(String[] args) {
+        Woman woman = new Woman();
+       Cat cat = new Cat();
+       cat.owner = woman;
+       Dog dog = new Dog();
+       dog.owner = woman;
+       Fish fish = new Fish();
+       fish.owner = woman;
+      }
+    public static class Cat {
+        public Woman owner;
+    }
+    public static class Dog {
+        public Woman owner;
+    }
+    public static class Fish {
+        public Woman owner;
+    }
+    public static class Woman {
+    }
+}
