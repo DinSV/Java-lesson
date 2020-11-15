38eecb4ab336caebee8cefcd21b7c7bfcafe613f
DinSV <63643468+DinSV@users.noreply.github.com>
2020-11-16 01:31:14 +0700

    Update Solution.java
    Обновка солюшн, этот файл будет отныне как конспект

--- Solution.java
+++ Solution.java
@@ -27,3 +27,20 @@ public class Solution {
     public static class Woman {
     }
 }
+//Путь к папке c JDK нужно указать в IDEA в разделе Project SDK. File -> Project Structure -> Project SDK -> Add Jdk
+/* 
+Делиться полезно
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        div(6, 3);
+        div(10, 6);
+        div(2, 4);
+    }
+
+    public static void div(int a, int b) { //реализация метода div
+        int c = a / b;
+        System.out.println(c); //без return - полсле него программа останавливается.
+    } // где то тут пора подключать учебники ибо на JavaRush многое  не обьясняется....
+}
